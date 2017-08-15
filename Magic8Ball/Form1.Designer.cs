46bd8a860fb1f31c8a49cb3e2c0c71c33ad9bf05
Abel Berhane <aberhane@datadimensions.com>
2017-08-15 09:44:53 -0400

    First commit

---
+++ Magic8Ball/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Magic8Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.askButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Magic8Ball.Properties.Resources._15;
+            this.pictureBox1.Location = new System.Drawing.Point(54, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(447, 384);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(68, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(421, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Ask your question... If you dare...";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // askButton
+            // 
+            this.askButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.askButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.askButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.askButton.Location = new System.Drawing.Point(171, 457);
+            this.askButton.Name = "askButton";
+            this.askButton.Size = new System.Drawing.Size(205, 62);
+            this.askButton.TabIndex = 2;
+            this.askButton.Text = "ASK";
+            this.askButton.UseVisualStyleBackColor = false;
+            this.askButton.Click += new System.EventHandler(this.askButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Magic8Ball.Properties.Resources.Wallpaper;
+            this.ClientSize = new System.Drawing.Size(559, 538);
+            this.Controls.Add(this.askButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Magic 8 Ball";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button askButton;
+    }
+}
+
---
+++ Magic8Ball/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8Ball
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //The Magic for the ball is done all in this button.
+        private void askButton_Click(object sender, EventArgs e)
+        {
+            //Call the random class
+            Random rnd = new Random();
+
+            //Create a variable that holds a random number from 1-15
+            int num = rnd.Next(1, 15);
+
+            //Assign the Image source to the resource folder with 8 ball images labeled _1 - _15
+            pictureBox1.Image = Properties.Resources._1;
+
+            //Switch the image based on the number that shows up
+            switch(num)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._6;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources._7;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources._8;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources._9;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources._10;
+                    break;
+                case 11:
+                    pictureBox1.Image = Properties.Resources._11;
+                    break;
+                case 12:
+                    pictureBox1.Image = Properties.Resources._12;
+                    break;
+                case 13:
+                    pictureBox1.Image = Properties.Resources._13;
+                    break;
+                case 14:
+                    pictureBox1.Image = Properties.Resources._14;
+                    break;
+                case 15:
+                    pictureBox1.Image = Properties.Resources._15;
+                    break;
+            }
+        }
+    }
+}
